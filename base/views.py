0aeceb76813dd1c954bb0f2f0f78e264be0c0c98
Smith Ringtho <sringtho@gmail.com>
2021-12-09 21:49:02 +0300

    ADD: tasks view homepage

--- base/views.py
+++ base/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from . models import Task
 
 def taskList(request):
-    return HttpResponse('To do list')
+    tasks = Task.objects.all()
+    context = {"tasks":tasks}
+    return render(request,'base/task_list.html', context)
