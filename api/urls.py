eff36e07f58f297c78a422a68fff82cdce902e8e
Smith Ringtho <sringtho@gmail.com>
2021-12-24 16:19:34 +0300

    ADD: POST functions for creating and updating a task

--- api/urls.py
+++ api/urls.py
@@ -4,4 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.getRoutes),
     path('tasks/', views.getTasks),
+    path('tasks/<str:pk>/', views.getTask),
+    path('tasks-create/',views.createTask),
+    path('tasks-update/<str:pk>/',views.updateTask),
+    path('tasks-delete/<str:pk>/',views.deleteTask),
 ]
--- api/views.py
+++ api/views.py
@@ -20,3 +20,33 @@ def getTasks(request):
     tasks = Task.objects.all()
     serializer = TaskSerializer(tasks, many=True)
     return Response(serializer.data)
+
+@api_view(['GET'])
+def getTask(request, pk):
+    task = Task.objects.get(id=pk)
+    if task:
+        serializer = TaskSerializer(task, many=False)
+        return Response(serializer.data)  
+    else:
+        return Response(f"The task with the id {pk} does not exist!")
+
+@api_view(['POST'])
+def createTask(request):
+    serializer = TaskSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def updateTask(request,pk):
+    task = Task.objects.get(id=pk)
+    serializer = TaskSerializer(instance = task, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
+
+@api_view(['DELETE'])
+def deleteTask(request,pk):
+    task = Task.objects.get(id=pk)
+    task.delete()
+    return Response("Task successfully deleted!")
